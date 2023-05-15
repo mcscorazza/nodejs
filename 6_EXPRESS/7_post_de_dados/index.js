2f66f6c26093afe77fc283cadea70fcf0de1ff99
Marcos Corazza <mcs.corazza@gmail.com>
2023-05-15 09:32:05 -0300

    correção do express

--- 6_EXPRESS/7_post_de_dados/index.js
+++ 6_EXPRESS/7_post_de_dados/index.js
@@ -1,12 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 const path = require('path')
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 const basePath = path.join(__dirname, 'templates')
@@ -15,15 +13,17 @@ app.get('/users/add', (req,res) => {
     res.sendFile(`${basePath}/userForm.html`)
 })
 
-app.post('/users/save', (res,req) => {
-    console.log("AAA", req.body)
+app.post('/users/save', (req,res) => {
+    console.log(({requestBody: req.body}))
+    const name = req.body.name
+    const age = req.body.age
+    console.log(`O nome do Usuário é ${name} e ele tem ${age} anos.`)
+    res.send(`O nome do Usuário é ${name} e ele tem ${age} anos.`)
 })
 
 app.get('/users/:id', (req,res) => {
     const id = req.params.id
-
     console.log(`Estamos buscando pelo usuario: ${id}`)
-
     res.sendFile(`${basePath}/users.html`)
 })
 
