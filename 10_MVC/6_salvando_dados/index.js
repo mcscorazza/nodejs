ffb9cbfd4cf296cd9aa4376236cc8678b97a61b2
Marcos Corazza <mcs.corazza@gmail.com>
2023-06-07 06:57:32 -0300

    Atualização 07-06-2023

---
+++ 10_MVC/6_salvando_dados/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import exphbs from 'express-handlebars'
+import { db } from './db/conn.js'
+import {router} from './routes/tasksRoutes.js'
+
+import { Task } from './models/Task.js'
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+app.use('/tasks',router)
+
+db
+    .sync(
+        //{force:true}
+        )
+    .then(() => { 
+        app.listen(3000)
+    })
+    .catch((err) => console.log(err))
---
+++ 10_MVC/6_salvando_dados/routes/tasksRoutes.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import TaskController from '../controllers/TaskController.js'
+export const router = express.Router()
+
+router.get('/add', TaskController.createTask)
+router.post('/add', TaskController.createTaskSave)
+router.get('/', TaskController.showTasks)
