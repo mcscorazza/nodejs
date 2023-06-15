dbdca011513799e19a57bc2f99c270a1041b0c2f
Marcos Corazza <mcs.corazza@gmail.com>
2023-06-15 11:16:47 -0300

    Atualizacao 15-06-23 office

---
+++ 10_MVC/8_removendo_dados/models/Task.js
@@ -0,0 +1,17 @@
+import {DataTypes} from 'sequelize'
+import {db} from '../db/conn.js'
+
+export const Task = db.define('Task', {
+    title: {
+        type: DataTypes.STRING,
+        required: true
+    },
+    description: {
+        type: DataTypes.STRING,
+        required: true
+    },
+    done: {
+        type: DataTypes.BOOLEAN,
+        required: true
+    },
+})
---
+++ 10_MVC/8_removendo_dados/routes/tasksRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import TaskController from '../controllers/TaskController.js'
+export const router = express.Router()
+
+router.get('/add', TaskController.createTask)
+router.post('/add', TaskController.createTaskSave)
+router.post('/remove', TaskController.removeTask)
+router.get('/', TaskController.showTasks)
